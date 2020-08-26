501f460a6b5a069a70fefec750a880efba8463d6
bilalcaliskan <bilalcaliskan@protonmail.com>
2020-08-26 19:55:44 +0300

    initial commit

---
+++ src/main/java/com/bcaliskan/springbeginnertoguru/SpringBeginnerToGuruApplication.java
@@ -0,0 +1,13 @@
+package com.bcaliskan.springbeginnertoguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerToGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeginnerToGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcaliskan/springbeginnertoguru/SpringBeginnerToGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcaliskan.springbeginnertoguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeginnerToGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
